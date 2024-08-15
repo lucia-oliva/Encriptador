63fce2a30bab510f9801aa2b495f61acca9b7842
Lucia Oliva <lucia222lr@gmail.com>
2024-08-14 22:11:14 -0300

    agregando funcionalidad
    funcion changeMode

--- scripts.js
+++ scripts.js
@@ -174,3 +174,58 @@ function ocultarAlerta(){
   // Añadir el evento click al botón
 const button_copy = document.getElementById('copiar_btn');
 button_copy.addEventListener('click', copiarAlPortapapeles);
+
+//funcion cambiar de modo 
+
+function changeMode(){
+ const modo = document.getElementById('modo');
+ var github = document.getElementById('github');
+ var alura = document.getElementById('alura-icon');
+ var linkedin = document.getElementById('linkedin icon');
+ var lock = document.getElementById('lock');
+ var unlock = document.getElementById('unlock');
+ var copy = document.getElementById('copy');
+
+ var root = document.documentElement;
+ if(modo.getAttribute("src") == "src/img/moon-ligth.svg"){
+    //change icons dark mode
+    modo.setAttribute("src", "src/img/sun-dark.svg");
+    github.setAttribute("src", "src/img/github-dark.svg");
+    alura.setAttribute("src", "src/img/alura-icon-dark.svg");
+    linkedin.setAttribute("src", "src/img/linkedin-dark.svg");
+    lock.setAttribute("src", "src/img/lock-dark.svg");
+    unlock.setAttribute("src", "src/img/unlock-dark.svg");
+    copy.setAttribute("src", "src/img/copy-dark.svg");
+    //change colors dark mode
+    root.style.setProperty('--first-color', '#8c426d');
+    root.style.setProperty('--second-color', '#2c1f26');
+    root.style.setProperty('--third-color', '#fff0f6');
+    root.style.setProperty('--fourth-color', '#fff0f6');
+    root.style.setProperty('--fifth-color', '#2c1f26');
+    root.style.setProperty('--hover-color', '#b94589');
+    root.style.setProperty('--shadow-color', "rgba(11, 7, 9, 0.4)");
+ }else if(modo.getAttribute("src") == "src/img/sun-dark.svg"){
+     //change icons ligth mode
+     modo.setAttribute("src", "src/img/moon-ligth.svg");
+     github.setAttribute("src", "src/img/github-ligth.svg");
+     alura.setAttribute("src", "src/img/alura-icon-ligth.svg");
+     linkedin.setAttribute("src", "src/img/linkedin-ligth.svg");
+     lock.setAttribute("src", "src/img/lock-ligth.svg");
+     unlock.setAttribute("src", "src/img/unlock-ligth.svg");
+     copy.setAttribute("src", "src/img/copy-ligth.svg");
+     //change colors ligth mode
+     root.style.setProperty('--first-color', '#ff9ecd');
+     root.style.setProperty('--second-color', '#fff0f6');
+     root.style.setProperty('--third-color', '#34113f');
+     root.style.setProperty('--fourth-color', '#8c426d');
+     root.style.setProperty('--fifth-color', '#2c1f26');
+     root.style.setProperty('--hover-color', '#f074b0');
+     root.style.setProperty('--shadow-color', "rgb(44, 31, 38 ,0.4)");
+ }
+}
+
+
+//setear funcion cambiar de color a un boton
+
+const button_mode = document.getElementById('mode_btn');
+button_mode.addEventListener('click', changeMode);
